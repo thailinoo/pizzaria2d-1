4600fc364380c065b8e0c555951cdff8cdcf578d
thailinoo <thaila.lino45@gmail.com>
2024-08-05 22:06:30 -0300

    tudo vai acabar em pizza

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Inicio from "./pages/Inicio";
-
-
+import {useState} from 'react'
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Listagem from './pages/Listagem';
 const App = () => {
-
-  const [color] = useState("#ebddff")
-  document.body.style.backgroundColor = color
-
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/inicio" element={<Inicio />} />◘
-    </Routes>
-  )
+const [color] = useState("#0000FF")
+document.body.style.backgroundColor = color
+return (
+<Routes>
+<Route path="/" element={<Listagem />}/>
+</Routes>
+)
 }
-
-export default App
+export default App;
---
+++ src/assets/Pizzas.jsx
@@ -0,0 +1,12 @@
+import mussarela from "../assets/images/mussarela.png"
+import calabresa from "../assets/images/calabresa.png"
+import atum from "../assets/images/atum.png"
+import portuguesa from "../assets/images/portuguesa.png"
+import frango from "../assets/images/frango.png"
+
+    const Pizzas = [
+        {
+            "id": 1,
+            "nome" : "Mussarela"
+        }
+    ]
