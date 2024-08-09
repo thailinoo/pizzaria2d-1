e68845d5f8ec9621ecdd8c18d6079b946d1c169b
thailinoo <thaila.lino45@gmail.com>
2024-08-08 22:41:35 -0300

    tudo acabara em pizza

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import {useState} from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import Listagem from './pages/Listagem';
 const App = () => {
-const [color] = useState("#0000FF")
-document.body.style.backgroundColor = color
 return (
 <Routes>
-<Route path="/" element={<Listagem />}/>
+<Route path="/" />
 </Routes>
 )
 }
-export default App;
